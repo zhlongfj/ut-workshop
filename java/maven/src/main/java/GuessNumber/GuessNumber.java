c55746ab639addaba1eb23ad4c6005295305897f
zhlongfj <zhlongfj@sina.com>
2015-02-02 11:42:11 +0800

    test gettips, return 0A4B, when input is 1234 and answer is 4321

--- java/maven/src/main/java/GuessNumber/GuessNumber.java
+++ java/maven/src/main/java/GuessNumber/GuessNumber.java
@@ -5,10 +5,18 @@ package GuessNumber;
  */
 public class GuessNumber {
     public String getTips(String input, String answer) {
+        int containCounts = 0;
         if (input.equals(answer)) {
             return "4A0B";
         } else {
-            return null;
+            for (int i = 0; i < answer.length(); i++) {
+                if (input.contains(String.valueOf(answer.charAt(i)))) {
+                    containCounts ++;
+                }
+            }
+
+            String tips = "0A" + containCounts + "B";
+            return tips;
         }
     }
 }
--- java/maven/src/test/java/GuessNumberTest.java
+++ java/maven/src/test/java/GuessNumberTest.java
@@ -8,11 +8,26 @@ import static org.fest.assertions.api.Assertions.assertThat;
  */
 public class GuessNumberTest {
     @Test
-    public void hello_world_test(){
+    public void get_tips_test_return_4A0B_when_input_is_1234_and_answer_is_1234(){
         //given
         String input = "1234";
         String answer = "1234";
         String tipsExpect = "4A0B";
+
+        //when
+        GuessNumber guessNumber = new GuessNumber();
+        String tips = guessNumber.getTips(input, answer);
+
+        //then
+        assertThat(tips).isEqualTo(tipsExpect);
+    }
+
+    @Test
+    public void get_tips_test_return_0A4B_when_input_is_1234_and_answer_is_4321(){
+        //given
+        String input = "1234";
+        String answer = "4321";
+        String tipsExpect = "0A4B";
 
         //when
         GuessNumber guessNumber = new GuessNumber();
