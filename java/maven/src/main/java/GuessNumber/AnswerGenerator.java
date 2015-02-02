0c4d012e0e956ef2927f6dd12877e2c31bb8f2c6
zhlongfj <zhlongfj@sina.com>
2015-02-02 22:03:51 +0800

    test generateARandomDigitWithoutRepeat, return a string that is consist of number

--- java/maven/src/main/java/GuessNumber/AnswerGenerator.java
+++ java/maven/src/main/java/GuessNumber/AnswerGenerator.java
@@ -1,10 +1,21 @@
 package GuessNumber;
 
+import java.util.Random;
+
 /**
  * Created by zhl on 15/2/2.
  */
 public class AnswerGenerator {
     public String generateARandomDigitWithoutRepeat() {
-        return "1234";
+        StringBuffer randomDigit = new StringBuffer();
+        String str = "0123456789";
+        Random r = new Random();
+        for (int i = 0; i < 4; i++) {
+            int num = r.nextInt(str.length());
+            randomDigit.append(str.charAt(num));
+            str = str.replace((str.charAt(num)+""), "");
+        }
+
+        return randomDigit.toString();
     }
 }
--- java/maven/src/test/java/AnswerGeneratorTest.java
+++ java/maven/src/test/java/AnswerGeneratorTest.java
@@ -21,5 +21,25 @@ public class AnswerGeneratorTest {
         assertThat(digit.length()).isEqualTo(4);
     }
 
-    
+    @Test
+    public void generate_a_random_digit_without_repeat_test_is_a_digit_string(){
+        //given
+        int number = 0;
+
+        //when
+        AnswerGenerator answerGenerator = new AnswerGenerator();
+        String digit = answerGenerator.generateARandomDigitWithoutRepeat();
+
+        //then
+        boolean isDigit = true;
+        for (int i = 0; i < digit.length(); i++) {
+            if (!Character.isDigit(digit.charAt(i))) {
+                isDigit = false;
+                break;
+            }
+        }
+
+        assertThat(isDigit).isEqualTo(true);
+    }
+
 }
