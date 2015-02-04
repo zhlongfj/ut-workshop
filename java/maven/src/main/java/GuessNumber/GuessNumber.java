bc64526b4c19deabb722d9e3a790349a50a948d4
zhlongfj <zhlongfj@sina.com>
2015-02-04 14:00:36 +0800

    Replace Temp with Query to method: getTips in class: GuessNumber

--- java/maven/src/main/java/GuessNumber/GuessNumber.java
+++ java/maven/src/main/java/GuessNumber/GuessNumber.java
@@ -5,19 +5,30 @@ package GuessNumber;
  */
 public class GuessNumber {
     public String getTips(String input, String answer) {
+        int countsOfCotain = getCountsOfContain(input, answer);
+        int countsOfEuqal = getCountsOfEqual(input, answer);
+
+        return getTipsFromCountsOfContainAndCountsOfEuqal(countsOfCotain, countsOfEuqal);
+    }
+
+    private int getCountsOfContain(String input, String answer) {
         int countsOfCotain = 0;
-        int countsOfEuqal = 0;
         for (int i = 0; i < answer.length(); i++) {
             if (input.contains(String.valueOf(answer.charAt(i)))) {
                 countsOfCotain++;
             }
+        }
+        return countsOfCotain;
+    }
 
+    private int getCountsOfEqual(String input, String answer) {
+        int countsOfEuqal = 0;
+        for (int i = 0; i < answer.length(); i++) {
             if (input.charAt(i) == answer.charAt(i)) {
                 countsOfEuqal++;
             }
         }
-
-        return getTipsFromCountsOfContainAndCountsOfEuqal(countsOfCotain, countsOfEuqal);
+        return countsOfEuqal;
     }
 
     private String getTipsFromCountsOfContainAndCountsOfEuqal(int countsOfContain, int countsOfEqual) {
