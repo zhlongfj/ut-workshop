d2081e3eb219e65d082aec982c56a46ccfc567db
zhlongfj <zhlongfj@sina.com>
2015-02-06 16:35:00 +0800

    implement the requirement when the player guess the right number

--- java/maven/src/main/java/Guess.java
+++ java/maven/src/main/java/Guess.java
@@ -5,6 +5,11 @@
 public class Guess {
     public String guessNumber(String input, AnswerGenerator answerGenerator) {
         GuessNumber guessNumber = new GuessNumber();
-        return guessNumber.getTips(input, answerGenerator.generate());
+        String result = guessNumber.getTips(input, answerGenerator.generate());
+        if (result.equals("4A0B")) {
+            result = "Congratulations!";
+        }
+
+        return result;
     }
 }
--- java/maven/src/test/java/GuessTest.java
+++ java/maven/src/test/java/GuessTest.java
@@ -11,12 +11,24 @@ public class GuessTest {
     @Test
     public void should_return_0A4B_when_input_is_1234_mock_answer_is_4321() {
         String input = "1234";
-
         AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+
         when(answerGenerator.generate()).thenReturn("4321");
         Guess guess = new Guess();
         String result = guess.guessNumber(input, answerGenerator);
 
         assertThat(result).isEqualTo("0A4B");
+    }
+
+    @Test
+    public void should_return_when_input_is_1234_mock_answer_is_1234() {
+        String input = "1234";
+
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn("1234");
+        Guess guess = new Guess();
+        String result = guess.guessNumber(input, answerGenerator);
+
+        assertThat(result).isEqualTo("Congratulations!");
     }
 }
