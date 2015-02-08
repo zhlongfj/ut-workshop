0ba9ba4b36bfcf89093244afeb0d60b15026624c
zhlongfj <zhlongfj@sina.com>
2015-02-08 14:22:07 +0800

    refactor, extract game and out

--- java/maven/src/test/java/GameProcessTest.java
+++ java/maven/src/test/java/GameProcessTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InOrder;
 
@@ -9,25 +10,33 @@ import static org.mockito.Mockito.*;
  * Created by zhl on 15/2/7.
  */
 public class GameProcessTest {
+
+    private PrintStream out;
+    private GameProcess game;
+
+    @Before
+    public void setUp() throws Exception {
+        out = mock(PrintStream.class);
+        game = new GameProcess(out);
+    }
+
     @Test
     public void should_print_welcome_when_game_start() {
-        PrintStream out = mock(PrintStream.class);
-        GameProcess game = new GameProcess(out);
-
-        verify(out, never()).print("Welcome!");
+        verify(out, never()).println("Welcome!");
         game.start();
-        verify(out).print("Welcome!");
+        verify(out).println("Welcome!");
     }
 
     @Test
     public void should_print_please_input_after_game_start() {
-        PrintStream out = mock(PrintStream.class);
-        GameProcess game = new GameProcess(out);
-
         game.start();
         InOrder inOrder = inOrder(out);
         inOrder.verify(out).println("Welcome!");
         inOrder.verify(out).println("Please input your number(6): ");
+    }
+
+    @Test
+    public void should_() {
 
     }
 }
